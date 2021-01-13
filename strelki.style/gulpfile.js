a8a7d08509e43d311d72a796a4e9d9520fd32355
Yura <fanbearytb@gmail.com>
2021-01-13 15:44:00 +0200

    add strelki project

---
+++ strelki.style/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'); // все данные из модуля node->gulp в переменную; 
+var sass = require('gulp-sass'); // инициализация обработчика gulp->sass
+var uglify = require('gulp-uglify');
+// var browserSync = require('browser-sync');
+
+// npm install gulp-sass --save-dev установка gulp
+// npm install browser-sync --save-dev установка авторелоад;
+// npm install --save-dev gulp gulp-uglify
+// npm install --save-dev gulp browserify
+
+gulp.task('sass', function() {
+	return gulp.src('app/sass/chaspick.sass')
+		.pipe(sass())
+		.pipe(gulp.dest('./dist/css'))
+});
+
+gulp.task('js', function() {
+  return gulp.src('app/js/common.js')
+    .pipe(uglify())
+    .pipe(gulp.dest('./dist/js'))
+});
+
+gulp.task('watch', function(){
+	gulp.watch('app/sass/chaspick.sass', gulp.series('sass'));
+	gulp.watch('app/js/common.js', gulp.series('js'));
+})
+
+  // другие ресурсы
+// gulp.task('watch', ['browserSync', 'sass'], function(){
+// 	gulp.watch('app/sass/chaspick.sass', ['sass'])
+// })
